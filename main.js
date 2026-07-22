94c052d47f2bb156cae803d85c5d6b19c1e29461
Splamei <instantveemo@gmail.com>
2026-07-22 16:42:03 +0100

    Adjusted the about dialog
    Signed-off-by: Splamei <instantveemo@gmail.com>

--- main.js
+++ main.js
@@ -564,7 +564,7 @@ function showAboutDialog(parentWindow) {
         type: 'info',
         title: "About - Rhythm Plus Splamei Client",
         message: "About the client",
-        detail: `A client to play the Rhythm Plus music game in an app right on your device\n\nVersion: ${app.getVersion()}\nNode: ${process.versions.node}\nElectron: ${process.versions.electron}\n\nMade with <3 by Splamei`,
+        detail: `A client to play the Rhythm Plus music game in an app right on your device\n\n----\n\nClient: ${app.getVersion()}\n\nNode: ${process.versions.node}\nElectron: ${process.versions.electron}\nChromium: ${process.versions.chrome}\nV8: ${process.versions.v8}\n\n----\n\nMade with <3 by Splamei`,
         buttons: ['OK']
     });
 }
